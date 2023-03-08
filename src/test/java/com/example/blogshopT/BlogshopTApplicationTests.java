8dbd03bff10b19b3d8bf1c94a4364b78d90481bb
itsangewthing <yann.ngew@gmail.com>
2023-03-08 21:12:50 +0800

    first commit
    added pom.xml , added folders, added server

---
+++ src/test/java/com/example/blogshopT/BlogshopTApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.blogshopT;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlogshopTApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
